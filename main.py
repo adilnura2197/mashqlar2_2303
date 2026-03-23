68205ae5a319ca524fecdddb25016eedbb5ac142
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-03-23 17:31:04 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+#11
+summa = lambda a, b, c: a + b + c
+
+res = summa(4, 6, 8)
+print(res)
+
+
+#12
+avg = lambda a, b, c: (a + b + c) // 3
+
+res = avg(12, 3, 15)
+print(res)
+
+
+#13
+pow = lambda x: x ** 5
+
+res = pow(2)
+print(res)
+
+
+#14
+add = lambda a, b: a * b + 10
+
+res = add(2, 2)
+print(res)
+
+
+#15
+kvadrat = lambda x: x * x
+
+res = kvadrat(2)
+print(res)
+
+
+#16
+calc = lambda x: x * 100 + 50
+
+res = calc(21)
+print(res)
+
+
+#17
+sum_kv = lambda a, b: a*a + b*b
+
+res = sum_kv(2, 6)
+print(res)
+
+
+#18
+half_square = lambda x: (x / 2) ** 2
+
+res = half_square(12)
+print(res)
+
+
+#19
+expr = lambda x: (x + 7) * 3
+
+res = expr(2)
+print(res)
+
+
+#20
+mul3 = lambda a, b, c: a * b * c
+
+res = mul3(2, 4, 6)
+print(res)
